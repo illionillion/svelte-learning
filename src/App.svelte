aafd607ab229055cfdac9ad4ecd4e8de5c41990e
yuto ichinose <yuto.0526.sano@gmail.com>
2025-09-13 16:36:25 +0900

    feat: カウントアプリ実装

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Count from './component/count/index.svelte';
+
+  import './app.css';
+</script>
+
+<main class="main-container">
+  <h1 class="main-title">Hello, Svelte!</h1>
+  <Count />
+</main>
---
+++ src/component/count/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+  function decrement() {
+    if(count === 0) return;
+    count -= 1;
+  }
+  import './index.css';
+</script>
+
+<div class="counter-area">
+  <div class="counter-value">{count}</div>
+  <div class="button-group">
+    <button class="btn-increment" on:click={increment}>Increment</button>
+    <button class="btn-decrement" on:click={decrement}>Decrement</button>
+  </div>
+</div>
