9f9e671e56bda0a8d0239da5b22af5be82f833e5
yuto ichinose <yuto.0526.sano@gmail.com>
2025-09-14 11:36:37 +0900

    feat: not-found実装

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Router from "svelte-spa-router";
   import Home from "./route/home/index.svelte";
   import Todo from "./route/todo/index.svelte";
+  import NotFound from "./route/not-found/index.svelte";
 
   const routes = {
     "/": Home,
     "/todo": Todo,
+    "*": NotFound,
   };
 </script>
 
---
+++ src/route/not-found/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "./index.css";
+</script>
+
+<main class="notfound-container">
+  <h1 class="notfound-title">404 - Page Not Found</h1>
+  <a href="#/">Go to Home</a>
+</main>
