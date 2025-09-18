e62d896321420db8719e675a4a21666604141679
yuto ichinose <yuto.0526.sano@gmail.com>
2025-09-19 00:09:50 +0900

    feat: GitHub Pagesデプロイ用のワークフローを追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'docs', // 出力先をdocsに
+  },
+  base: process.env.GITHUB_PAGES ? '/svelte-learning/' : '/'
 })
